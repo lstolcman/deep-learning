160071dafda34b29cc7e1de6fdb04e2fd625de6f
Łukasz Stolcman <v0lt.lucas@gmail.com>
2018-05-10 11:55:25 +0200

    lab1: partial soluton (only 1st assignment)

---
+++ lab1/tf_logreg.py
@@ -0,0 +1,127 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+import data
+import tensorflow as tf
+
+
+class TFLogreg:
+    def __init__(self, D, C, param_delta=0.5):
+        """Arguments:
+           - D: dimensions of each datapoint 
+           - C: number of classes
+           - param_delta: training step
+        """
+
+        # declare graph nodes for the data and parameters:
+        # self.X, self.Yoh_, self.W, self.b
+        self.X = tf.placeholder(tf.float64, [None, D])
+        self.Yoh_ = tf.placeholder(tf.float64, [None,C])
+        self.W = tf.Variable(np.array([np.random.randn(D) for i in range(C)])) # weights == columns == number of features # CxD
+        self.b = tf.Variable(np.array([0.0 for i in range(C)]))
+        
+        # formulate the model: calculate self.probs
+        #   use tf.matmul, tf.nn.softmax
+        scores = tf.matmul(self.X, self.W, transpose_b=True) + self.b
+        self.probs = tf.nn.softmax(scores)
+        
+        # formulate the loss: self.loss
+        #   use tf.log, tf.reduce_sum, tf.reduce_mean
+        # L(W,b|D)=∑_i−logP(Y=y_i|x_i)
+        ##minuslog = -tf.log(self.probs)
+        ##sumlog = tf.reduce_sum(minuslog)
+        ##self.loss = sumlog
+        self.loss = tf.losses.log_loss(self.Yoh_, self.probs)
+
+        # formulate the training operation: self.train_step
+        #   use tf.train.GradientDescentOptimizer,
+        #       tf.train.GradientDescentOptimizer.minimize
+        opt = tf.train.GradientDescentOptimizer(0.1) # defining step
+        self.train_step = opt.minimize(self.loss) # minimizing loss
+
+        # instantiate the execution context: self.session
+        #   use tf.Session
+        self.session = tf.Session()
+
+
+    def train(self, X, Yoh_, param_niter):
+        """Arguments:
+           - X: actual datapoints [NxD]
+           - Yoh_: one-hot encoded labels [NxC]
+           - param_niter: number of iterations
+        """
+        # parameter intiailization
+        #   use tf.global_variables_initializer !! deprecated
+        self.session.run(tf.global_variables_initializer())
+
+        data_in = {self.X: X, self.Yoh_: Yoh_}
+        # optimization loop
+        #   use tf.Session.run
+        ## tf.Session = self.session, so self.session.run()
+        for i in range(param_niter):
+            loss_val = self.session.run(self.loss, feed_dict=data_in)
+            self.session.run(self.train_step, feed_dict=data_in)
+            if i%100==0:
+                print('{} loss: {}'.format(i, loss_val))
+        print('{} loss: {}'.format(i, loss_val))
+
+
+    def eval(self, X):
+        """Arguments:
+           - X: actual datapoints [NxD]
+           Returns: predicted class probabilites [NxC]
+        """
+        #   use tf.Session.run
+        ## tf.Session = self.session, so self.session.run()
+        return self.session.run(self.probs, feed_dict={self.X: X})
+
+
+
+
+if __name__ == "__main__":
+    # initialize the random number generator
+    np.random.seed(100)
+    tf.set_random_seed(100)
+    tf.reset_default_graph() 
+
+    # instantiate the data X and the labels Yoh_
+    X = np.array([[1,3],[1,1],[3,2],[3,3]])
+    Y_ = np.array([1,0,2,2])
+    Yoh_ = np.zeros((len(X), len(np.bincount(Y_))))
+    Yoh_[range(len(Y_)), Y_] = 1
+    ## Yoh_s = tf.one_hot(D,C)
+    
+    '''
+    N_classes = 3
+    N_examples = 2 # examples from each class, so 3*10=30 in total
+    X, Y_ = data.sample_gauss_2d(N_classes, N_examples)
+    Yoh_ = np.zeros((N_examples*N_classes, N_classes))
+    Yoh_[range(len(Y_)), Y_] = 1
+    Yoh_ = tf.one_hot(Y_, N_classes)
+    '''
+
+    # build the graph:
+    tflr = TFLogreg(X.shape[1], Yoh_.shape[1], 0.5)
+
+    # perform the training with given hyper-parameters:
+    tflr.train(X, Yoh_, 1000)
+
+    # predict probabilities of the data points
+    probs = tflr.eval(X)
+
+    # print performance (per-class precision and recall)
+
+    # draw results, decision surface
+    decfun = lambda x: tflr.eval(x).argmax(axis=1)
+    bbox=(np.min(X-1, axis=0), np.max(X+1, axis=0))
+    data.graph_surface(decfun, bbox, offset=0.5)
+    '''
+    ## assign one color to each class
+    import matplotlib
+    c = matplotlib.cm.rainbow(np.linspace(0, 1, len(np.bincount(Y_))))
+    for i, p in enumerate(X):
+        plt.scatter(p[0], p[1], color=c[Y_[i]])
+    '''
+    Y = [np.argmax(p) for p in probs]
+    data.graph_data(X, Y_, Y)
+    plt.show()
